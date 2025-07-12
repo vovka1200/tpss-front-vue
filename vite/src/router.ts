a98e73a2a6e244054171591dd00a0eea787aac96
Vladimir Yavorskiy <vovka@krevedko.su>
2025-07-13 02:10:10 +0300

    variable removed

--- vite/src/router.ts
+++ vite/src/router.ts
@@ -1,12 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from './pages/Home.vue'
 
 export default createRouter({
     history: createWebHistory(),
     routes: [
         {
             path: '/',
-            component: Home,
+            component: () => import('./pages/Home.vue'),
         },
         {
             path: '/about',
