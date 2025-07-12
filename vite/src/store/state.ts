92c166b6949cf39f72e7f00e8222e7e48189bc58
Vladimir Yavorskiy <vovka@krevedko.su>
2025-07-13 02:10:46 +0300

    authorize dialog visible state access state api version

--- vite/src/store/state.ts
+++ vite/src/store/state.ts
@@ -1,23 +1,37 @@
+import {JSONRPCResponse} from "json-rpc-2.0";
+
 export interface State {
     socket: {
-        isConnected: boolean,
-        message: string,
+        isConnected: boolean
+        message: JSONRPCResponse | null
         reconnectError: boolean,
-        heartBeatInterval: number,
+        heartBeatInterval: number
         heartBeatTimer: number
     }
     backEndConnected: boolean
     backEndVersion: string
+    authDialogVisible: boolean
+    apiVersion: string
+    access: {
+        username: string
+        password: string
+    }
 }
 
-export const state = {
+export const defaultState: State = {
     socket: {
         isConnected: false,
-        message: "",
+        message: null,
         reconnectError: false,
-        heartBeatInterval: 5000,
+        heartBeatInterval: 60000,
         heartBeatTimer: 0
     },
-    backEndVersion: "",
+    backEndVersion: '',
     backEndConnected: false,
+    authDialogVisible: false,
+    apiVersion: '-',
+    access: {
+        username: '',
+        password: '',
+    }
 }
