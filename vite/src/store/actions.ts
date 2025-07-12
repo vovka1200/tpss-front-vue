f397deabd520de13f17b5c2016ec92995960296b
Vladimir Yavorskiy <vovka@krevedko.su>
2025-07-13 02:07:36 +0300

    login action

--- vite/src/store/actions.ts
+++ vite/src/store/actions.ts
@@ -1,5 +1,25 @@
 import {ActionContext} from "vuex";
 import {State} from "./state";
+import app from "../main";
+import {JSONRPC, JSONRPCRequest} from "json-rpc-2.0";
 
-export const fetch = function (context: ActionContext<State, State>) {
+export const login = function (context: ActionContext<State, State>) {
+    if (context.state.socket.isConnected) {
+        let jsonRPCRequest: JSONRPCRequest = {
+            jsonrpc: JSONRPC,
+            id: 1,
+            method: "login",
+            params: {
+                "username": context.state.access.username,
+                "password": context.state.access.password,
+            }
+        };
+        app.config.globalProperties.$socket.sendObj(jsonRPCRequest);
+        jsonRPCRequest = {
+            jsonrpc: JSONRPC,
+            id: 1,
+            method: "version",
+        };
+        app.config.globalProperties.$socket.sendObj(jsonRPCRequest);
+    }
 };
