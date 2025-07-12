372829c4bdd60a919e3c14d47b6ae33542ec0974
Vladimir Yavorskiy <vovka@krevedko.su>
2025-07-13 02:09:35 +0300

    icons

--- vite/src/components/Drawer.vue
+++ vite/src/components/Drawer.vue
@@ -24,49 +24,49 @@ defineProps({
 
         <q-item clickable v-ripple>
           <q-item-section avatar>
-            <q-icon name="people"/>
+            <q-icon name="o_people"/>
           </q-item-section>
           <q-item-section> Клиенты</q-item-section>
         </q-item>
 
         <q-item clickable v-ripple>
           <q-item-section avatar>
-            <q-icon name="article"/>
+            <q-icon name="o_article"/>
           </q-item-section>
           <q-item-section> Контракты</q-item-section>
         </q-item>
 
         <q-item clickable v-ripple>
           <q-item-section avatar>
-            <q-icon name="location_on"/>
+            <q-icon name="o_location_on"/>
           </q-item-section>
           <q-item-section> Адреса</q-item-section>
         </q-item>
 
         <q-item clickable v-ripple>
           <q-item-section avatar>
-            <q-icon name="widgets"/>
+            <q-icon name="o_widgets"/>
           </q-item-section>
           <q-item-section> Объекты</q-item-section>
         </q-item>
 
         <q-item clickable v-ripple>
           <q-item-section avatar>
-            <q-icon name="task"/>
+            <q-icon name="o_task"/>
           </q-item-section>
           <q-item-section> Задачи</q-item-section>
         </q-item>
 
         <q-item clickable v-ripple>
           <q-item-section avatar>
-            <q-icon name="account_tree"/>
+            <q-icon name="o_account_tree"/>
           </q-item-section>
           <q-item-section> Сценарии</q-item-section>
         </q-item>
 
         <q-item clickable v-ripple>
           <q-item-section avatar>
-            <q-icon name="analytics"/>
+            <q-icon name="o_analytics"/>
           </q-item-section>
           <q-item-section> Отчёты</q-item-section>
         </q-item>
