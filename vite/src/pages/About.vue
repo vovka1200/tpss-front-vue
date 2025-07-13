30eecdbb7dd449e96f23c56f257428d40b9ecbd3
Vladimir Yavorskiy <vovka@krevedko.su>
2025-07-13 18:48:35 +0300

    gui version on about page

--- vite/src/pages/About.vue
+++ vite/src/pages/About.vue
@@ -12,11 +12,14 @@ const version = computed(() => {
   <q-page padding class="text-black">
     <h5>Task Tracker for Production, Sale and Support</h5>
     <p>Система управления заданиями по производству, продажи и сопровождения</p>
-    <q-separator class="q-my-md"/>
-    <ul>
-      <li>Версия GUI:&nbsp;1.0.0</li>
-      <li>Версия API:&nbsp;{{ version }}</li>
-    </ul>
+    <q-card>
+      <q-card-section>
+        <ul>
+          <li>Версия GUI:&nbsp;1.0.0</li>
+          <li>Версия API:&nbsp;{{ version }}</li>
+        </ul>
+      </q-card-section>
+    </q-card>
   </q-page>
 </template>
 
