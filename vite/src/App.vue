2899c8db23fd8dcf485e985c2fb323ce35c04869
Vladimir Yavorskiy <vovka@krevedko.su>
2025-07-16 22:59:15 +0300

    toolbar

--- vite/src/App.vue
+++ vite/src/App.vue
@@ -3,18 +3,42 @@ import {RouterView} from 'vue-router'
 import Drawer from './components/Drawer.vue'
 import {ref, computed} from "vue";
 import Authorize from "./components/Authorize.vue";
-import {useStore} from "./store";
-import Header from "./components/Header.vue";
+import {useStore} from "@/store";
+
+const openDrawer = ref(true)
 
 const store = useStore();
-const openDrawer = ref(true)
+const panel = computed(() => {
+  return store.state.panel
+});
 
 </script>
 
 <template>
   <q-layout view="hHh LpR fFf">
 
-    <Header v-bind:model-value="openDrawer"/>
+    <q-header elevated class="text-dark bg-secondary">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="openDrawer=!openDrawer"/>
+        <q-toolbar-title shrink>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-black.svg">
+          </q-avatar>
+          T.P.S.S.
+        </q-toolbar-title>
+        <q-toolbar-title>{{ panel }}</q-toolbar-title>
+        <q-space/>
+        <q-btn dense flat icon="o_account_circle" @click="$router.push('/account')">
+          <q-tooltip> Настройки пользователя</q-tooltip>
+        </q-btn>
+        <q-btn dense flat icon="o_info" @click="$router.push('/about')">
+          <q-tooltip>О программе</q-tooltip>
+        </q-btn>
+        <q-btn dense flat icon="o_settings" @click="$router.push('/settings')">
+          <q-tooltip>Панель управления</q-tooltip>
+        </q-btn>
+      </q-toolbar>
+    </q-header>
 
     <Drawer :visible="openDrawer"/>
 
