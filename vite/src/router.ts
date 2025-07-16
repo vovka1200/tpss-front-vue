e2bdfbea37e73f39ae66a21ef6f8091eade1174e
Vladimir Yavorskiy <vovka@krevedko.su>
2025-07-16 22:56:03 +0300

    settings and clients

--- vite/src/router.ts
+++ vite/src/router.ts
@@ -1,6 +1,8 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import About from './pages/About.vue'
 import Account from "./pages/Account.vue";
+import Clients from "./pages/Clients.vue";
+import Settings from "./pages/Settings.vue";
 
 export default createRouter({
     history: createWebHistory(),
@@ -16,6 +18,14 @@ export default createRouter({
         {
             path: '/account',
             component: Account,
+        },
+        {
+            path: '/clients',
+            component: Clients,
+        },
+        {
+            path: '/settings',
+            component: Settings,
         },
     ],
 })
