1c816be3ea0868195d421f800282c3decbcdb938
Vladimir Yavorskiy <vovka@krevedko.su>
2025-07-16 22:58:40 +0300

    users list

--- vite/src/store/state.ts
+++ vite/src/store/state.ts
@@ -1,5 +1,21 @@
 import {JSONRPCResponse} from "json-rpc-2.0";
 
+export interface Rule {
+    object: string
+    method: string
+}
+
+export interface User {
+    id: string
+    name: string
+    groups: string[]
+}
+
+export interface Group {
+    id: string
+    name: string
+}
+
 export interface State {
     socket: {
         isConnected: boolean
@@ -12,11 +28,24 @@ export interface State {
     backEndVersion: string
     authDialogVisible: boolean
     apiVersion: string
+    panel: string,
     access: {
         account: {
+            id: string
             username: string
             password: string
             name: string
+        },
+        matrix: Rule[]
+    },
+    settings: {
+        access: {
+            users: {
+                list: User[]
+            }
+            groups: {
+                list: Group[]
+            }
         }
     }
 }
@@ -33,12 +62,24 @@ export const defaultState: State = {
     backEndConnected: false,
     authDialogVisible: false,
     apiVersion: '-',
+    panel: '',
     access: {
         account: {
+            id: '',
             name: '',
             username: '',
             password: '',
-        }
+        },
+        matrix: [],
     },
-
+    settings: {
+        access: {
+            users: {
+                list: []
+            },
+            groups: {
+                list: []
+            },
+        }
+    }
 }
