a9e14c812724b455b4f5c7c3e47df51108cd0011
Vladimir Yavorskiy <vovka@krevedko.su>
2025-07-20 23:44:18 +0300

    User card

---
+++ vite/src/components/settings/access/groups/Card.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+
+import Objects from "@/components/settings/access/groups/Objects.vue";
+import {PropType, ref} from "vue";
+import {Group} from "@/models/access/groups";
+
+const visible = defineModel<Boolean>();
+const objectsTable = ref(false);
+defineProps({
+  data: {
+    type: Object as PropType<Group>,
+    required: true,
+  },
+});
+
+</script>
+
+<template>
+  <q-dialog v-bind:model-value="visible" class="text-dark" @hide="visible=false">
+    <q-card>
+      <q-toolbar>
+        <q-toolbar-title>Группа</q-toolbar-title>
+        <q-space/>
+        <q-btn flat dense icon="o_lock_reset" @click="objectsTable=true">
+          <q-tooltip>Настройки доступа</q-tooltip>
+        </q-btn>
+      </q-toolbar>
+
+      <q-separator/>
+
+      <q-card-section>
+        <q-item>
+          <q-input v-bind:model-value="data?.name" label="Название" label-color="dark"/>
+        </q-item>
+      </q-card-section>
+
+      <q-separator/>
+
+      <q-card-actions align="right" class="text-dark">
+        <q-btn flat class="bg-positive" label="Сохранить"/>
+        <q-btn flat class="bg-accent" label="Отмена" @click="visible=false"/>
+      </q-card-actions>
+
+    </q-card>
+  </q-dialog>
+
+  <Objects v-model="objectsTable"/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ vite/src/components/settings/access/users/Card.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+
+import {User} from "@/models/access/users";
+import {PropType} from "vue";
+
+const visible = defineModel<Boolean>();
+
+defineProps({
+  data: {
+    type: Object as PropType<User>,
+    required: true,
+  },
+})
+
+</script>
+
+<template>
+  <q-dialog v-bind:model-value="visible" class="text-dark" @hide="visible=false">
+    <q-card>
+
+      <q-card-section>
+        <div class="text-h6">Пользователь</div>
+      </q-card-section>
+
+      <q-separator/>
+
+      <q-card-section>
+        <q-item>
+          <q-input v-bind:model-value="data?.username" label="Имя(логин)" label-color="dark"/>
+        </q-item>
+        <q-item>
+          <q-input type="password" model-value="" label="Пароль" label-color="dark"/>
+        </q-item>
+        <q-item>
+          <q-input v-bind:model-value="data?.name" label="ФИО" label-color="dark"/>
+        </q-item>
+      </q-card-section>
+
+      <q-card-actions align="right" class="text-dark">
+        <q-btn flat class="bg-positive" label="Сохранить"/>
+        <q-btn flat class="bg-accent" label="Отмена" @click="visible=false"/>
+      </q-card-actions>
+
+    </q-card>
+  </q-dialog>
+</template>
+
+<style scoped>
+
+</style>
