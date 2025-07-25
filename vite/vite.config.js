18d61dd776a411d369eb005dbe260014296d59d4
Vladimir Yavorskiy <vovka@krevedko.su>
2025-07-25 22:43:18 +0300

    PWA

--- vite/vite.config.js
+++ vite/vite.config.js
@@ -1,12 +1,40 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import {quasar} from '@quasar/vite-plugin'
+import {VitePWA} from 'vite-plugin-pwa'
 import path from 'path'
 
 export default defineConfig({
     plugins: [
         vue(),
         quasar(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                id: 'TPSS',
+                name: 'Tracker for Production, Sale and Support',
+                short_name: 'T.P.S.S.',
+                description: 'High customizing issues/tasks tracker for production, sale and support with ERP/CRM features.',
+                icons: [
+                    {
+                        src: '/tpss32.png',
+                        sizes: '32x32',
+                        type: 'image/png',
+                    },
+                    {
+                        src: '/tpss144.png',
+                        sizes: '144x144',
+                        type: 'image/png',
+                    },
+                ],
+            },
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,ico,png,svg,webmanifest}'],
+            },
+            devOptions: {
+                enabled: true,
+            },
+        }),
     ],
     resolve: {
         alias: {
