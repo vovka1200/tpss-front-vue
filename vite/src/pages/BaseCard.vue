f51e5a94d352097c67501bc8de3dcc952078daee
Vladimir Yavorskiy <vovka@krevedko.su>
2025-07-25 22:48:31 +0300

    client base card

---
+++ vite/src/pages/BaseCard.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+
+defineProps({
+  drawer_head_height: String
+})
+
+</script>
+
+<template>
+  <q-page class="q-pa-sm">
+    <div class="cols">
+      <div class="q-pa-sm">
+        <slot name="card"/>
+      </div>
+      <q-drawer show-if-above side="right" bordered>
+        <div class="absolute-top bg-accent q-pa-sm base-drawer-head">
+          <slot name="drawer_head"/>
+        </div>
+        <q-scroll-area class="text-dark base-drawer-scroll">
+          <q-list>
+            <slot name="drawer_list"/>
+          </q-list>
+        </q-scroll-area>
+      </q-drawer>
+    </div>
+  </q-page>
+</template>
+
+<style scoped>
+
+.base-drawer-head {
+  color: var(--vt-c-white-soft);
+  height: v-bind('drawer_head_height');
+}
+
+.base-drawer-scroll {
+  height: calc(100% - v-bind('drawer_head_height') - 8px);
+  margin-top: calc(v-bind('drawer_head_height') + 8px);
+}
+
+</style>
---
+++ vite/src/pages/Client.vue
@@ -0,0 +1,78 @@
+<script setup lang="ts">
+
+import {useRoute} from "vue-router";
+import {ref} from "vue";
+import {useMainStore} from "@/store";
+import BaseCard from "@/pages/BaseCard.vue";
+
+const route = useRoute();
+const id = ref(route.params.id);
+
+const mainStore = useMainStore();
+mainStore.panel = 'Клиент';
+
+const options = ref([
+  'Активен', 'Удалён'
+]);
+
+const status = ref(null);
+
+</script>
+
+<template>
+  <BaseCard drawer_head_height="5em">
+    <template #drawer_head>
+      <q-select v-model="status" label="Статус" label-color="white" :options="options" dark>
+        <template v-slot:prepend>
+          <q-icon name="workspace_premium"/>
+        </template>
+      </q-select>
+    </template>
+    <template #drawer_list>
+      <q-item>test1</q-item>
+      <q-item>test2</q-item>
+      <q-item>test3</q-item>
+      <q-item>test4</q-item>
+      <q-item>test5</q-item>
+      <q-item>test6</q-item>
+      <q-item>test7</q-item>
+      <q-item>test8</q-item>
+      <q-item>test9</q-item>
+      <q-item>test0</q-item>
+      <q-item>test1</q-item>
+      <q-item>test2</q-item>
+      <q-item>test3</q-item>
+      <q-item>test4</q-item>
+      <q-item>test5</q-item>
+      <q-item>test6</q-item>
+      <q-item>test7</q-item>
+      <q-item>test8</q-item>
+      <q-item>test9</q-item>
+      <q-item>test0</q-item>
+      <q-item>test1</q-item>
+      <q-item>test2</q-item>
+      <q-item>test3</q-item>
+      <q-item>test4</q-item>
+      <q-item>test5</q-item>
+      <q-item>test6</q-item>
+      <q-item>test7</q-item>
+      <q-item>test8</q-item>
+      <q-item>test9</q-item>
+      <q-item>test0</q-item>
+      <q-item>test1</q-item>
+      <q-item>test2</q-item>
+      <q-item>test3</q-item>
+      <q-item>test4</q-item>
+      <q-item>test5</q-item>
+      <q-item>test6</q-item>
+      <q-item>test7</q-item>
+      <q-item>test8</q-item>
+      <q-item>test9</q-item>
+      <q-item>test011</q-item>
+    </template>
+  </BaseCard>
+</template>
+
+<style scoped>
+
+</style>
