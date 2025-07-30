a932289b8ef4aa2b2932a60ddaa04ada7279de1d
Vladimir Yavorskiy <vovka@krevedko.su>
2025-07-30 21:44:49 +0300

    custom params component

---
+++ vite/src/components/params/Card.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+
+import {Param, ParamNumber, ParamSelect, ParamString} from "@/models/params";
+
+const props = defineProps<{
+  params: Param[]
+}>();
+
+function isInteger(val: string) {
+  return val == undefined || val.length === 0 || /^\d+$/.test(val);
+}
+
+function isFloat(val: string) {
+  return val == undefined || val.length === 0 || /^\d+\.\d+$/.test(val);
+}
+
+</script>
+
+<template>
+  <div class="row">
+    <div v-for="param in props.params" class="q-px-xs col" style="flex: 0 0 25%;">
+      <q-input v-if="param.type==='string'"
+               v-model="(<ParamString>param).value"
+               :label="param.name"
+               type="text"
+      />
+      <q-input v-if="param.type==='int'"
+               v-model="(<ParamNumber>param).value"
+               :label="param.name"
+               type="number"
+               :rules="[val => isInteger(val) || 'Введите целое число']"
+      />
+      <q-input v-if="param.type==='float'"
+               v-model="(<ParamNumber>param).value"
+               :label="param.name"
+               type="number"
+               mask="#.##"
+               :rules="[val => isFloat(val) || 'Введите дробное число']"
+      />
+      <q-select v-if="param.type==='select'"
+                v-model="(<ParamSelect>param).value"
+                :label="param.name"
+                use-input
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
