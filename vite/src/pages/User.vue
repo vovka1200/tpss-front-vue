30cf36b8d1873258d0826b4c4b342bc641813dd5
Vladimir Yavorskiy <vovka@krevedko.su>
2025-08-11 21:03:45 +0300

    dates display

--- vite/src/pages/User.vue
+++ vite/src/pages/User.vue
@@ -1,81 +1,149 @@
 <script setup lang="ts">
 import BaseCard from "@/pages/BaseCard.vue";
-import {computed, ref, watch} from "vue";
+import {computed, ref, shallowRef, watch} from "vue";
 import {useRoute} from "vue-router";
 import {useUsersStore} from "@/store/access/users";
-import CardParams from '@/components/params/Card.vue';
-import {Param} from "@/models/params";
+import ParamsSection from '@/components/params/CardSection.vue';
 import {useWebsocketStore} from "@/store/websocket";
-
-const test = ref('');
+import {useAccountStore} from "@/store/access/account";
+import {Method} from "@/models/access/maxrix";
+import {Notify} from "quasar";
+import {date} from 'quasar'
 
 const id = useRoute().params.id as string;
+const store = useUsersStore();
+const user = shallowRef(store);
 
-const data = computed(() => useUsersStore().user(id));
+const readAllowed = computed(() => useAccountStore().allowed('User'));
+const writeAllowed = computed(() => useAccountStore().allowed('User', Method.write));
+
+const tab = ref('tasks');
+const datetime = ref('2019/02/01');
 
 watch(() => useWebsocketStore().authorized, (ok) => {
-  if (ok && useUsersStore().list.length === 0) {
-    useUsersStore().load(id);
+  if (ok) {
+    if (!readAllowed.value) {
+      Notify.create({
+        icon: 'block',
+        color: 'negative',
+        message: `Доступ ограничен`,
+      });
+      return;
+    }
+    if (!writeAllowed.value) {
+      Notify.create({
+        icon: 'block',
+        color: 'warning',
+        message: `Доступ ограничен`,
+      });
+    }
+    if (useUsersStore().list.length === 0) {
+      useUsersStore().load(id);
+    }
   }
 });
-
-const params = ref<Param[]>([
-  {name: 'test1', type: 'string'},
-  {name: 'test2', type: 'string'},
-  {name: 'test3', type: 'string'},
-  {name: 'test6', type: 'string'},
-  {name: 'test7', type: 'select'},
-  {name: 'test6', type: 'string'},
-  {name: 'test6', type: 'string'},
-  {name: 'test6', type: 'string'},
-  {name: 'test6', type: 'string'},
-  {name: 'test6', type: 'string'},
-  {name: 'test6', type: 'string'},
-  {name: 'test6', type: 'string'},
-  {name: 'test6', type: 'string'},
-  {name: 'test6', type: 'string'},
-  {name: 'test8', type: 'int'},
-  {name: 'test9', type: 'float'},
-  {name: 'test10', type: 'string'},
-]);
 
 </script>
 
 <template>
-  <BaseCard drawer_head_height="5em">
+  <BaseCard drawer_head_height="5em" v-if="readAllowed">
     <template #card>
       <q-card>
         <q-toolbar>
           <q-toolbar-title>Редактирование пользователя</q-toolbar-title>
           <q-space/>
-          <q-btn flat color="positive" label="Сохранить" icon="save"/>
-          <q-btn flat color="accent" label="Отмена" icon="cancel"/>
+          <q-btn flat icon="o_settings_suggest" v-if="writeAllowed">
+            <q-tooltip>Добавить параметр</q-tooltip>
+          </q-btn>
+          <q-separator vertical/>
+          <q-btn flat color="positive" icon="o_save" v-if="writeAllowed">
+            <q-tooltip>Сохранить</q-tooltip>
+          </q-btn>
+          <q-btn flat color="negative" icon="o_cancel" v-if="writeAllowed">
+            <q-tooltip>Отменить</q-tooltip>
+          </q-btn>
         </q-toolbar>
 
         <q-separator/>
         <div class="row" style="flex-wrap: nowrap;">
           <q-card-section style="flex-shrink: 0;">
             <q-item-label>Основные данные</q-item-label>
-            <q-input v-bind:model-value="data?.username" label="Имя(логин)"/>
+            <q-input v-bind:model-value="user?.get(id)?.username" label="Имя(логин)"/>
             <q-input type="password" model-value="" label="Пароль"/>
-            <q-input v-bind:model-value="data?.name" label="ФИО"/>
+            <q-input v-bind:model-value="user?.get(id)?.name" label="ФИО"/>
           </q-card-section>
-
+          <q-separator vertical/>
           <q-card-section>
             <q-item-label>Дополнительные параметры</q-item-label>
-            <CardParams :params="params"/>
+            <ParamsSection :params="user?.get(id)?.params"/>
           </q-card-section>
         </div>
 
       </q-card>
     </template>
     <template #drawer_head>
-      <q-select v-model="test" label="Статус" label-color="white" :options="[]" dark>
+      <q-select dark
+                :model-value="user?.get(id)?.statusId"
+                option-label="name"
+                option-value="id"
+                label="Статус"
+                label-color="white"
+                :options="[]"
+      >
         <template v-slot:prepend>
           <q-icon name="workspace_premium"/>
         </template>
       </q-select>
     </template>
+    <template #drawer_list>
+      <q-input readonly :model-value="date.formatDate(user.get(id)?.created,'DD.MM.YYYY')" label="Создан">
+        <template v-slot:append>
+          <q-icon name="event" class="cursor-pointer">
+            <q-popup-proxy cover transition-show="scale" transition-hide="scale">
+              <q-date :model-value="date.formatDate(user.get(id)?.created,'YYYY/MM/DD')">
+                <div class="row items-center justify-end">
+                  <q-btn v-close-popup label="Закрыть" color="primary" flat/>
+                </div>
+              </q-date>
+            </q-popup-proxy>
+          </q-icon>
+        </template>
+      </q-input>
+      <q-input readonly :model-value="date.formatDate(user.get(id)?.updated,'DD.MM.YYYY')" label="Обновлён">
+        <template v-slot:append>
+          <q-icon name="event" class="cursor-pointer">
+            <q-popup-proxy cover transition-show="scale" transition-hide="scale">
+              <q-date :model-value="date.formatDate(user.get(id)?.updated,'YYYY/MM/DD')">
+                <div class="row items-center justify-end">
+                  <q-btn v-close-popup label="Закрыть" color="primary" flat/>
+                </div>
+              </q-date>
+            </q-popup-proxy>
+          </q-icon>
+        </template>
+      </q-input>
+    </template>
+    <template #bottom>
+      <q-card>
+        <q-tabs
+            v-model="tab"
+            dense
+            active-color="text-dark"
+            indicator-color="text-grey"
+            align="left"
+            narrow-indicator
+        >
+          <q-tab name="tasks" label="Задачи"/>
+          <q-tab name="location" label="Геолокация"/>
+        </q-tabs>
+        <q-separator/>
+        <q-tab-panels v-model="tab" animated>
+          <q-tab-panel name="tasks">tests</q-tab-panel>
+          <q-tab-panel name="location">test2222</q-tab-panel>
+        </q-tab-panels>
+      </q-card>
+    </template>
+
   </BaseCard>
 </template>
 <style scoped>
