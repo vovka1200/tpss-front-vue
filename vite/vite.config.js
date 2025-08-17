24a8f017a8ff8e50e5f2a7e02226f2bf29e4e8d3
Vladimir Yavorskiy <vovka@krevedko.su>
2025-08-17 08:39:14 +0300

    debug log removed

--- vite/vite.config.js
+++ vite/vite.config.js
@@ -12,32 +12,11 @@ export default defineConfig({
                 changeOrigin: true,
                 secure: false,
                 rewrite: (path) => path.replace(/file/, 'api/v1/file'),
-                configure: (proxy, _options) => {
-                    proxy.on("error", (err, _req, _res) => {
-                        console.log("proxy error", err);
-                    });
-                    proxy.on("proxyReq", (proxyReq, req, _res) => {
-                        console.log(
-                            "Sending Request:",
-                            req.method,
-                            req.url,
-                            " => TO THE TARGET =>  ",
-                            proxyReq.method,
-                            proxyReq.protocol,
-                            proxyReq.host,
-                            proxyReq.path,
-                            JSON.stringify(proxyReq.getHeaders()),
-                        );
-                    });
-                    proxy.on("proxyRes", (proxyRes, req, _res) => {
-                        console.log(
-                            "Received Response from the Target:",
-                            proxyRes.statusCode,
-                            req.url,
-                            JSON.stringify(proxyRes.headers),
-                        );
-                    });
-                },
+            },
+            '/api/v1/users': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+                secure: false,
             },
         },
     },
