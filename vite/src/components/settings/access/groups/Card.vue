8d698035abfd4d48cb818b868cfa2bfb9c756aef
Vladimir Yavorskiy <vovka@krevedko.su>
2025-08-18 22:23:15 +0300

    user group card

--- vite/src/components/settings/access/groups/Card.vue
+++ vite/src/components/settings/access/groups/Card.vue
@@ -1,54 +1,100 @@
 <script setup lang="ts">
 
-import {PropType} from "vue";
-import {Group} from "@/models/access/groups";
+import {computed, ref, watch} from "vue";
 import Rules from "@/components/settings/access/groups/Rules.vue";
+import BaseCard from "@/pages/BaseCard.vue";
+import {useAccountStore} from "@/store/access/account";
+import {Method} from "@/models/access/maxrix";
+import {storeToRefs} from "pinia";
+import {useCloned} from "@vueuse/core";
+import {useWebsocketStore} from "@/store/websocket";
+import {Notify} from "quasar";
+import {useGroupsStore} from "@/store/access/groups";
 
-const visible = defineModel<Boolean>();
+const tab = ref('rules');
+const store = useGroupsStore();
+const {item} = storeToRefs(store);
+const {cloned, sync} = useCloned(item);
 
-defineProps({
-  data: {
-    type: Object as PropType<Group>,
-    required: true,
-  },
+const props = defineProps({
+  id: String,
+});
+
+const readAllowed = computed(() => useAccountStore().allowed('Group') || true);
+const writeAllowed = computed(() => useAccountStore().allowed('Group', Method.write));
+
+watch(() => useWebsocketStore().authorized, (ok) => {
+  if (ok) {
+    if (!readAllowed.value) {
+      Notify.create({
+        icon: 'block',
+        color: 'negative',
+        message: `Доступ ограничен`,
+      });
+      return;
+    }
+    if (!writeAllowed.value) {
+      Notify.create({
+        icon: 'block',
+        color: 'warning',
+        message: `Доступ ограничен`,
+      });
+    }
+    store.load(props.id);
+  }
 });
 
 </script>
 
 <template>
-  <q-dialog v-bind:model-value="visible" @hide="visible=false">
-    <q-card style="min-width: 50vw; min-height: 54vh;">
-      <q-toolbar>
-        <q-toolbar-title>Редактирование группы</q-toolbar-title>
-        <q-space/>
-        <q-btn flat dense color="positive" icon="o_save">
-          <q-tooltip>Сохранить</q-tooltip>
-        </q-btn>
-        <q-btn flat dense color="accent" icon="o_refresh">
-          <q-tooltip>Отменить</q-tooltip>
-        </q-btn>
-        <q-btn flat dense color="negative" icon="o_cancel">
-          <q-tooltip>Закрыть</q-tooltip>
-        </q-btn>
-      </q-toolbar>
+  <BaseCard drawer_head_height="5em" v-if="readAllowed">
+    <template #card>
+      <q-card>
+        <q-toolbar>
+          <q-toolbar-title>Редактирование группы</q-toolbar-title>
+          <q-space/>
+          <q-btn flat dense color="positive" icon="o_save">
+            <q-tooltip>Сохранить</q-tooltip>
+          </q-btn>
+          <q-btn flat dense color="accent" icon="o_refresh">
+            <q-tooltip>Отменить</q-tooltip>
+          </q-btn>
+          <q-btn flat dense color="negative" icon="o_cancel" @click="$emit('onClose')">
+            <q-tooltip>Закрыть</q-tooltip>
+          </q-btn>
+        </q-toolbar>
 
-      <q-separator/>
+        <q-separator/>
 
-      <q-card-section>
-        <q-item>
-          <q-input v-bind:model-value="data?.name" label="Название"/>
-        </q-item>
-      </q-card-section>
+        <q-card-section>
+          <q-item>
+            <q-input v-bind:model-value="cloned?.name" label="Название"/>
+          </q-item>
+        </q-card-section>
 
-      <q-separator/>
-
-      <q-card-section>
-        <q-item-label>Правила доступа</q-item-label>
-        <Rules v-bind:group-id="data.id"/>
-      </q-card-section>
-
-    </q-card>
-  </q-dialog>
+      </q-card>
+    </template>
+    <template #bottom>
+      <q-card>
+        <q-tabs
+            v-model="tab"
+            dense
+            active-color="text-dark"
+            indicator-color="text-grey"
+            align="left"
+            narrow-indicator
+        >
+          <q-tab name="rules" label="Правила доступа"/>
+        </q-tabs>
+        <q-separator/>
+        <q-tab-panels v-model="tab" animated>
+          <q-tab-panel name="rules">
+            <Rules v-bind:id="cloned.id"/>
+          </q-tab-panel>
+        </q-tab-panels>
+      </q-card>
+    </template>
+  </BaseCard>
 </template>
 
 <style scoped>
